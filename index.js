ca806825c64a22d424a8077dfbf37d2e8466f38f
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-07-28 10:37:55 -0300

    init -2

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 exports.complete = (start, char, line) => {
     try {
-        for (i = false; i == true; i = i) {
+        for (i = 0; i == line.length; i++) {
             if (/[\n|\n\r]/.test(line)) {
-                i = true;
-                console.log(line);
                 return line;
             } else {
-                var line = line.splice(line.indexOf(start), 0, char);
+                line = line.splice(line.indexOf(start), 0, char);
             }
         }
     } catch (error) {
