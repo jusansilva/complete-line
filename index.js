e431801e8226568f1a5d32e443ec4e89210142b8
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-07-28 15:43:51 -0300

    complete project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.complete = function(start, char, str, strLength) {
     try {
         var NewStr ='';
         for (var i = 0; i < strLength; i++) {
-            if (line.length > 54) {
+            if (str.length > 54) {
                 return str;
             } else {
                 str = NewStr.concat(str.slice(0, str.indexOf(start) + start.length),char, str.slice(str.indexOf(start) + start.length, str.length));
